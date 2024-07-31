0b086c1c05a02d35df130cbc3db6021a1e95036c
andrew <160489710+andrvew@users.noreply.github.com>
2024-07-31 04:44:59 -0400

    Update and rename List.json to List.lua

---
+++ Projects/ZC7/Core/List.lua
@@ -0,0 +1,4 @@
+return {
+    [1008451066] = {Name = "Da Hood", Adonis = false, Argument = {Array = 1, Arg = 2}},
+    [3634139746] = {Name = "Hood Customs", Adonis = false, Argument = {Name = "MousePosUpdate", Array = 1, Arg = 2}},
+}
