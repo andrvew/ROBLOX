d882a791224727c2743f2f08337a2969a619c827
andrew <160489710+andrvew@users.noreply.github.com>
2024-08-02 01:57:24 -0400

    Fixed

--- Other/EmbedManager.lua
+++ Other/EmbedManager.lua
@@ -2,6 +2,7 @@ local HttpService = game:GetService("HttpService")
 
 local Embed = {}
 Embed.__index = Embed
+
 do
     function Embed.new(username, avatar_Url)
         local self = setmetatable({}, Embed)
@@ -12,7 +13,7 @@ do
 
         return self
     end
-
+    
     function Embed:CreateEmbed(title, description, color)
         local embed = {
             title = title or "",
@@ -24,55 +25,54 @@ do
             footer = {},
             fields = {}
         }
-    
         table.insert(self.embeds, embed)
 
         return embed
+    end    
+end
+
+do
+    function Embed:SetThumbnail(embed, url)
+        embed.thumbnail.url = url or ""
     end
-
-    do
-        function Embed:SetThumbnail(embed, url)
-            embed.thumbnail.url = url or ""
-        end
-
-        function Embed:SetAuthor(embed, name, icon_url)
-            embed.author.name = name or ""
-            embed.author.icon_url = icon_url or ""
-        end
-
-        function Embed:SetFooter(embed, text, icon_url)
-            embed.footer.text = text or ""
-            embed.footer.icon_url = icon_url or ""
-        end
-
-        function Embed:SetTimestamp(embed, timestamp)
-            embed.timestamp = timestamp and DateTime.now():ToIsoDate() or nil
-        end
-
-        function Embed:AddField(embed, name, value, inline)
-            local field = {
-                name = name or "",
-                value = value or "",
-                inline = inline or true
-            }
-            table.insert(embed.fields, field)
-        end
+    
+    function Embed:SetAuthor(embed, name, icon_url)
+        embed.author.name = name or ""
+        embed.author.icon_url = icon_url or ""
     end
+    
+    function Embed:SetFooter(embed, text, icon_url)
+        embed.footer.text = text or ""
+        embed.footer.icon_url = icon_url or ""
+    end
+    
+    function Embed:SetTimestamp(embed, timestamp)
+        embed.timestamp = timestamp and DateTime.now():ToIsoDate() or nil
+    end
+    
+    function Embed:AddField(embed, name, value, inline)
+        local field = {
+            name = name or "",
+            value = value or "",
+            inline = inline or true
+        }
+        table.insert(embed.fields, field)
+    end
+end
 
-    function Embed:SendEmbed(webhookUrl)
-        local response = request({
-            Url = webhookUrl,
-            Method = "POST",
-            Headers = {["Content-Type"] = "application/json"},
-            Body = HttpService:JSONEncode({
-                username = self.username,
-                avatar_url = self.avatar_url,
-                embeds = self.embeds
-            })
+function Embed:SendEmbed(webhookUrl)
+    local response = request({
+        Url = webhookUrl,
+        Method = "POST",
+        Headers = {["Content-Type"] = "application/json"},
+        Body = HttpService:JSONEncode({
+            username = self.username,
+            avatar_url = self.avatar_url,
+            embeds = self.embeds
         })
-        
-        return response
-    end
+    })
+
+    return response
 end
 
 return Embed
